4e6f09813f0ed960350cf942fa2574cd82d0cc92
Osiris Gonzalez <OsirisMango@Osiriss-MacBook-Pro.local>
2017-03-16 00:04:02 -0400

    first commit

---
+++ Codigo.py
@@ -0,0 +1,28 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1504\cocoasubrtf810
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 VitualEnvirotment\
+\
+$cd Desktop ( sis heiress poner tu virtual env en desktop\
+$ pip3.6 install virtualenv\
+#despues crear el virtual env\
+\
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+\cf0 $ virtuales myvirtualenv(nombre de la carpetavirtualenv)/cf_project(nombredelacarpeta)\
+\
+#activando virtuales\
+$ source myvirtualenv/cf_project/bin/active\
+\
+#cuando se hace eso usualmente se tienen: (cf_project)nombre-macbooketc: Desktop nombre de tu compu\
+\
+\
+\
+AHORA PARA DESCARGAR DJANGO\
+$ pip3.6 install django == 1.9.5\
+\
+#para saber si lo descargaste bien\
+$pip3.6 freeze}
